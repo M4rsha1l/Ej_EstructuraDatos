ba16f9fdbed9d7dcfbe9cea2a99abd1bce0edd65
yeyoe <yeyoe@DESKTOP-DIV1R4B>
2021-04-19 20:40:35 -0600

    Primer ejercicio, saludo

---
+++ src/ejerc_github/Ejerc_GitHub.java
@@ -0,0 +1,19 @@
+/*
+ * Proyecto generado en Netbeans para trabajar 
+ */
+package ejerc_github;
+
+/**
+ *
+ * @author yeyoe
+ */
+public class Ejerc_GitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola soy Marshallito");
+    }
+    
+}
